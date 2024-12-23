7313d2859a6a55b9c30ace4afbc3351fbbb9edf9
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2024-12-23 14:54:59 +0900

    Changes to be committed: 이?쁘게 수정 	modified:   main.py

--- main.py
+++ main.py
@@ -5,16 +5,23 @@ import numpy as np
 def f(x):
     return x**2
 
-#x = np.arange(0,10)
 x = np.linspace(0, 10, 20)
 point = np.linspace(0, 10, 100)
-plt.figure(figsize=(6,7))
-
+# matplotlib 폰트 설정
+plt.rcParams['font.family'] = ['Malgun Gothic', 'sans-serif']
+plt.rcParams['axes.unicode_minus'] = False
+plt.subplots(figsize=(6, 7))
 plt.plot(x,f(x))
+
 for i in range(point.size-1):
     position = (point[i],0)
     size = (point[i+1]-point[i], f(point[i+1]))
-    shp=patches.Rectangle(position, size[0], size[1], edgecolor = 'deeppink', facecolor = 'lightgray', fill=True,)
-    plt.gca().add_patch(shp)
+    rectangle = patches.Rectangle(position, size[0], size[1], edgecolor = 'deeppink', facecolor = 'lightgray', fill=True,)
+    plt.gca().add_patch(rectangle)
     plt.gca().text(position[0], size[1]/2, f'{round(size[0]*size[1],2)}')
+
+plt.title('구분구적법')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+
 plt.show()
